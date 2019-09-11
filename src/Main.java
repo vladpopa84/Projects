aa5667eb4212bae241ea2e6ecdc90462609145b1
Vlad Popa <vladpopa84@yahoo.com>
2019-09-11 20:54:08 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        int x = 2;
+        int y = 3;
+
+        int sum = x + y;
+
+        System.out.println(sum);
+
+        int z = 4;
+        float a= 3;
+
+        System.out.println(a*z);
+
+        String maker= "Dacia";
+        String colour= "white";
+
+
+
+
+    }
+
+
+    }
