36d8e338557871e49331cbe36c4d6c77d7ffc5c7
Vlad Popa <vladpopa84@yahoo.com>
2019-09-16 11:13:23 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,50 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        // Exercitiul 1
+        System.out.println("Hello!");
+        System.out.println("Numele meu este"+" "+"Vlad Popa"+".");
 
-        int x = 2;
-        int y = 3;
+        // Exercitiul 2
+        int x= 3;
+        float y= 2.56f;
+        System.out.println(x+y);
 
-        int sum = x + y;
+        // Exercitiul 3
 
-        System.out.println(sum);
+        float a= 2.7f;
+        float b= 3.4f;
+        float c= (a / b);
 
-        int z = 4;
-        float a= 3;
 
-        System.out.println(a*z);
+        System.out.println(c);
 
-        String maker= "Dacia";
-        String colour= "white";
+        // Exercitiul 4
+
+        // a. -5 + 8 * 6
+        byte z= -5 + (8*6);
+        System.out.println(z);
+
+        // b. (55+9) % 9
+        byte w= (55+9)%9;
+        System.out.println(w);
+
+        // c. 20 + -3*5 / 8
+        float v= 20 + (-3*5)/8f;
+        System.out.println(v);
+
+        // d. 5 + 15 / 3 * 2 - 8 % 3
+        int s= 5 + 15 / 3*2 - 8%3;
+        System.out.println(s);
+
+
+
+
+
+
+
+
+
 
 
 
