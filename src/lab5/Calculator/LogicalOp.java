66cbcb2da21a382fb173e5a0c8c2824b3966c502
Vlad Popa <vladpopa84@yahoo.com>
2019-10-26 00:20:02 +0300

    .

--- src/lab5/Calculator/LogicalOp.java
+++ src/lab5/Calculator/LogicalOp.java
@@ -11,9 +11,39 @@ public class LogicalOp {
         return sum;
     }
 
+    public float sum(float firstNumber, float secondNumber) {
+        float sum = firstNumber + secondNumber;
+        return sum;
+    }
+
+    public float sum(float firstNumber, float secondNumber, float thirdNumber) {
+        float sum = firstNumber + secondNumber + thirdNumber;
+        return sum;
+    }
+
+    public int sum(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) {
+        int sum = firstNumber + secondNumber + thirdNumber + fourthNumber;
+        return sum;
+    }
+
     //Scadere
     public int sub(int firstNumber, int secondNumber) {
         int sub = firstNumber - secondNumber;
+        return sub;
+    }
+
+    public float sub(float firstNumber, float secondNumber) {
+        float sub = firstNumber - secondNumber;
+        return sub;
+    }
+
+    public float sub(float firstNumber, float secondNumber, float thirdNumber) {
+        float sub = firstNumber - secondNumber - thirdNumber;
+        return sub;
+    }
+
+    public int sub(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) {
+        int sub = firstNumber - secondNumber - thirdNumber - fourthNumber;
         return sub;
     }
 
@@ -23,19 +53,67 @@ public class LogicalOp {
         return multy;
     }
 
+    public float multy(float firstNumber, float secondNumber) {
+        float multy = firstNumber * secondNumber;
+        return multy;
+    }
+
+    public float multy(float firstNumber, float secondNumber, float thirdNumber) {
+        float multy = firstNumber * secondNumber * thirdNumber;
+        return multy;
+    }
+
+    public int multy(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) {
+        int multy = firstNumber * secondNumber * thirdNumber * fourthNumber;
+        return multy;
+    }
+
     //Impartire
-    public float div(float firstNUmber, float secondNumber) {
-        float div = firstNUmber / secondNumber;
+    public float div(int firstNumber, int secondNumber) {
+        float div = firstNumber / secondNumber;
         return div;
     }
+
+    public float div(float firstNumber, float secondNumber) {
+        float div = firstNumber / secondNumber;
+        return div;
+    }
+
+    public float div(float firstNumber, float secondNumber, float thirdNumber) {
+        float div = firstNumber / secondNumber / thirdNumber;
+        return div;
+    }
+
+    public float div(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) {
+        float div = (float) firstNumber / secondNumber / thirdNumber / fourthNumber;
+        return div;
+    }
+
+
+//2.  In clasa LogicalOp, creati o metoda care sa scrie pe un array de 100 de pozitii, valorile de la 1 la 100. Sa se afiseze progresul in consola pe tot parcursul. Apelati metoda in main() pentru a verifica daca functioneaza.
+
+    public int[] oneToHundred() {
+        int[] myArray = new int[100];
+        for (int i = 1; i <= 100; i++) {
+            myArray[i - 1] = i;
+        }
+        return myArray;
+
+    }
+
+
+    //3. Creati o metoda care primeasca un parametru de tip array de numere intregi gol, si sa il returneze populat cu toate valorile pare de la 1 la 100. Apelati metoda in main() pentru a verifica daca functioneaza. Atentie, metoda returneaza un array, deci acesta va trebui surprins si afisat folosind o bucla.
+
+    public int[] evenNumbers(int[] array) {
+
+        int[] myArray = new int[50];
+        int index = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                myArray[index] = i;
+                index++;
+            }
+        }
+      return myArray;
+    }
 }
-
-
-
-
-
-
-
-
-
-
--- src/lab5/Calculator/Main.java
+++ src/lab5/Calculator/Main.java
@@ -1,17 +1,34 @@
 package lab5.Calculator;
 
 
-import lab4.Calculator.LogicalOp;
+import lab5.Calculator.LogicalOp;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         LogicalOp op = new LogicalOp();
+        int sum = op.sum(1, 3, 5, 5);
+        System.out.println(sum);
 
+        int sub = op.sub(1, 3, 5, 5);
+        System.out.println(sub);
+
+        int multy = op.multy(1, 3, 5, 5);
+        System.out.println(multy);
+
+        float div = op.div(8, 2, 2, 3);
+        System.out.println(div);
+
+
+        int[] myArray = op.oneToHundred();
+        System.out.println(Arrays.toString(myArray));
+
+
+        int[] even = op.evenNumbers(myArray);
+        System.out.println(Arrays.toString(even));
 
     }
-
-
-
 
 
 }
