194451f49e53995f3a04c89882cba57a6b29f5f7
Vlad Popa <vladpopa84@yahoo.com>
2019-11-09 19:55:15 +0200

    .

--- src/lab7/Calculator/Main.java
+++ src/lab7/Calculator/Main.java
@@ -25,26 +25,26 @@ public class Main {
 
         Read read = new Read();
 
-//        int a = read.returnInt();
-//        System.out.println(a);
-//
-//        float b = read.returnFloat();
-//        System.out.println(b);
-//
-//        long c = read.returnLong();
-//        System.out.println(c);
-//
-//        double d = read.returnDouble();
-//        System.out.println(d);
-//
-//        int[] myArray = read.returnArray(5);
-//        System.out.println(Arrays.toString(myArray));
+        int a = read.returnInt();
+        System.out.println(a);
 
-//        List<Integer>myList = new ArrayList<>();
-//
-//        read.returnList(myList);
+        float b = read.returnFloat();
+        System.out.println(b);
 
-//        read.positionValue(2);
+        long c = read.returnLong();
+        System.out.println(c);
+
+        double d = read.returnDouble();
+        System.out.println(d);
+
+        int[] myArray = read.returnArray(5);
+        System.out.println(Arrays.toString(myArray));
+
+        List<Integer>myList = new ArrayList<>();
+
+        read.returnList(myList);
+
+        read.positionValue(2);
 
 
 
